9c9802f011bd824566e2e9dc44500a2814b236a2
Brian Stephenson <brian@bstephen.me.uk>
2020-09-28 16:37:54 +0100

    Add files via upload

--- classol.d
+++ classol.d
@@ -13,56 +13,29 @@ uint check_classol(uint id) {
    string[] log;
    Connection conn;
    ResultRange range;
+   string sql_1 = "SELECT classol.pid FROM classol LEFT JOIN problem "
+      ~ "ON classol.pid = problem.pid "
+      ~ "WHERE problem.pid IS NULL " ~ "ORDER BY classol.pid";
 
    auto connectionStr = "host=localhost;port=3306;user=bstephen;pwd=rice37;db=meson";
    conn = new Connection(connectionStr);
+
+   /+	(1)	Check that the PID of every record in table Classol appears in table
+	 +			Problem.
+	 +/
+
+   range = conn.query(sql_1);
+
+   foreach (Row row; range) {
+      string mess = "PID " ~ to!string(row[0]) ~ " not in table Problem!";
+      log ~= mess;
+      rc++;
+   }
+
+   range.close();
    conn.close();
 
    display_status(id, log, "check_classol");
 
    return rc;
 }
-/+
-sub check_classol {
-    my ( $dbh, $rid, $tid ) = @_;
-    my $count = 0;
-    my $sth;
-    my $r_row;
-    my $pid;
-    my $ar;
-    my $mess;
-    my $ccsol_check_1 =
-        'SELECT classol.pid FROM classol LEFT JOIN problem '
-      . 'ON classol.pid = problem.pid '
-      . 'WHERE problem.pid IS NULL '
-      . 'ORDER BY classol.pid';
-
-    $ar = [ $rid, 1, 'Checking table Classol ...', $tid ];
-    $message_queue->enqueue($ar);
-
-    #print("\tChecking table Classol ...\n");
-
-    #	Check that the PID of every record in table Classol appears in table
-    #	Problem.
-
-    $sth = $dbh->prepare($ccsol_check_1);
-    $sth->execute();
-
-    while ( $r_row = $sth->fetchrow_arrayref ) {
-        $pid  = $r_row->[0];
-        $mess = sprintf 'PID %d not in table Problem!', $pid;
-        $ar   = [ $rid, 2, $mess ];
-        $message_queue->enqueue($ar);
-
-        #print("\t\tPID $pid not in table Problem!\n");
-        $count++;
-    }
-
-    $sth->finish();
-
-    $ar = [ $rid, 0 ];
-    $message_queue->enqueue($ar);
-
-    return $count;
-}
-+/
--- source.d
+++ source.d
@@ -13,77 +13,32 @@ uint check_source(uint id) {
    string[] log;
    Connection conn;
    ResultRange range;
+   ulong c_done;
+   string sql_1 = "CREATE TEMPORARY TABLE tempa SELECT source.sid, source.name FROM source LEFT JOIN problemsource " ~ "ON source.sid = problemsource.sid " ~ "WHERE problemsource.sid IS NULL " ~ "ORDER BY source.sid";
+   string sql_2 = "CREATE TEMPORARY TABLE tempb SELECT tempa.sid, tempa.name FROM tempa LEFT JOIN problem " ~ "ON tempa.sid = problem.sid " ~ "WHERE problem.sid IS NULL " ~ "ORDER BY tempa.sid";
+   string sql_3 = "SELECT tempb.sid, tempb.name FROM tempb LEFT JOIN jos_meson_magranges " ~ "ON tempb.sid = jos_meson_magranges.mid " ~ "WHERE jos_meson_magranges.mid IS NULL " ~ "ORDER BY tempb.sid";
 
    auto connectionStr = "host=localhost;port=3306;user=bstephen;pwd=rice37;db=meson";
    conn = new Connection(connectionStr);
+
+   /+	(1)	Check that the SID of every record in table Source is in table Problem or in table ProblemSource.
+	 +/
+
+   c_done = conn.exec(sql_1);
+   c_done = conn.exec(sql_2);
+   range = conn.query(sql_3);
+
+   foreach (Row row; range) {
+      string mess = "SID " ~ to!string(row[0]) ~ "(" ~ to!string(row[1])
+         ~ ") not in table ProblemSource, Problem or jos_meson_magranges!";
+      log ~= mess;
+      rc++;
+   }
+
+   range.close();
    conn.close();
 
    display_status(id, log, "check_source");
 
    return rc;
 }
-/+
-sub check_source {
-    my ( $dbh, $rid, $tid ) = @_;
-    my $count = 0;
-    my $sth;
-    my $r_row;
-    my $sid;
-    my $name;
-    my $ar;
-    my $mess;
-    my $csrc_check_1 =
-'CREATE TEMPORARY TABLE tempa SELECT source.sid, source.name FROM source LEFT JOIN problemsource '
-      . 'ON source.sid = problemsource.sid '
-      . 'WHERE problemsource.sid IS NULL '
-      . 'ORDER BY source.sid';
-    my $csrc_check_2 =
-'CREATE TEMPORARY TABLE tempb SELECT tempa.sid, tempa.name FROM tempa LEFT JOIN problem '
-      . 'ON tempa.sid = problem.sid '
-      . 'WHERE problem.sid IS NULL '
-      . 'ORDER BY tempa.sid';
-    my $csrc_check_3 =
-        'SELECT tempb.sid, tempb.name FROM tempb LEFT JOIN jos_meson_magranges '
-      . 'ON tempb.sid = jos_meson_magranges.mid '
-      . 'WHERE jos_meson_magranges.mid IS NULL '
-      . 'ORDER BY tempb.sid';
-
-    $ar = [ $rid, 1, 'Checking table Source ...', $tid ];
-    $message_queue->enqueue($ar);
-
-    #print("\tChecking table Source ...\n");
-
-#	Check that the SID of every record in table Source is in table Problem or in table ProblemSource.
-
-    $sth = $dbh->prepare($csrc_check_1);
-    $sth->execute();
-    $sth->finish();
-
-    $sth = $dbh->prepare($csrc_check_2);
-    $sth->execute();
-    $sth->finish();
-
-    $sth = $dbh->prepare($csrc_check_3);
-    $sth->execute();
-
-    while ( $r_row = $sth->fetchrow_arrayref ) {
-        $sid  = $r_row->[0];
-        $name = $r_row->[1];
-        $mess = sprintf
-'SID %d (%s) not in table ProblemSource, Problem or jos_meson_magranges!',
-          $sid, $name;
-        $ar = [ $rid, 2, $mess ];
-        $message_queue->enqueue($ar);
-
-#print("\t\tSID $sid ($name) not in table ProblemSource and not in table Problem!\n");
-        $count++;
-    }
-
-    $sth->finish();
-
-    $ar = [ $rid, 0 ];
-    $message_queue->enqueue($ar);
-
-    return $count;
-}
-+/
